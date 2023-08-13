8aa5e7b1258bc0049719edbe489153a4c8b1182c
jperezpenaranda <joperez@bancolombia.com.co>
2023-08-13 15:01:17 -0500

    Actualizacion experiencias

---
+++ src/components/recentWork/experiences.ts
@@ -0,0 +1,43 @@
+export const EXPERIENCES = [
+    {
+        projectName:'Pragma-Bancolombia - Podem/Geslegal',
+        role:'FullStack developer',
+        description:"I developed Fullstack applications with Angular, Java and SpringBoot, I managed and administered the Microsoft Azure suite, creation of: pipelines; realeases, I managed and administered AWS services such as S3, EC2, EKS, RDS, I also developed backend services with Java and Spring-boot.",
+        skills:[
+            {name:'Angular',urlImg:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png'},
+            {name:'Material Design',urlImg:'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c7/Google_Material_Design_Logo.svg/1024px-Google_Material_Design_Logo.svg.png'},
+            {name:'Java',urlImg:'https://assets.stickpng.com/images/58480979cef1014c0b5e4901.png'},
+            {name:'SpringBoot',urlImg:'https://cdn.freebiesupply.com/logos/large/2x/spring-3-logo-svg-vector.svg'},
+            {name:'Azure',urlImg:'https://swimburger.net/media/ppnn3pcl/azure.png'},
+            {name:'Aws',urlImg:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/1024px-Amazon_Web_Services_Logo.svg.png'},
+        ],
+        projectImg:'https://gruponutresa.com/wp-content/uploads/2016/09/capsulas-maquina-nutresabias-Grupo-Nutresa.jpg'
+    },
+    {
+        projectName:'Capsulas Express Nutresa',
+        role:'Frontend developer',
+        description:"I developed with my team the E-commerce of Nutresa Express Capsules using the VTEX framework. I developed both native vtex components and custom components with REACT.JS. Managed states with CONTEXT API. Consumed CMS services with AXIOS. Created testing environments. I supported eccommerce by managing content such as advertising guidelines, products, promotions and extra developments at the client's request.",
+        skills:[
+            {name:'Vtex',urlImg:'https://companieslogo.com/img/orig/VTEX-64045aa2.png?t=1635881259'},
+            {name:'React.js',urlImg:'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'},
+            {name:'Sass',urlImg:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/2560px-Sass_Logo_Color.svg.png'},
+            {name:'Redux',urlImg:'https://cdn.freebiesupply.com/logos/large/2x/redux-logo-png-transparent.png'},
+            {name:'Axios',urlImg:'https://user-images.githubusercontent.com/8939680/57233882-20344080-6fe5-11e9-9086-d20a955bed59.png'},
+            {name:'Aws',urlImg:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/1024px-Amazon_Web_Services_Logo.svg.png'},
+        ],
+        projectImg:'https://gruponutresa.com/wp-content/uploads/2016/09/capsulas-maquina-nutresabias-Grupo-Nutresa.jpg'
+    },
+    {
+        projectName:'Pragma - Vacaciones',
+        role:'Frontend developer',
+        description:"I developed user interfaces, modules and dynamic components that provide a better user experience with the implementation of technologies such as React.js, Context Api, StyledComponents, Axios and others. This in order to create a portal to manage the vacations of pragma's employees.",
+        skills:[
+            {name:'React.js',urlImg:'https://cdn4.iconfinder.com/data/icons/logos-3/600/React.js_logo-512.png'},
+            {name:'Sass',urlImg:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/96/Sass_Logo_Color.svg/2560px-Sass_Logo_Color.svg.png'},
+            {name:'Redux',urlImg:'https://cdn.freebiesupply.com/logos/large/2x/redux-logo-png-transparent.png'},
+            {name:'Axios',urlImg:'https://user-images.githubusercontent.com/8939680/57233882-20344080-6fe5-11e9-9086-d20a955bed59.png'},
+            {name:'Aws',urlImg:'https://upload.wikimedia.org/wikipedia/commons/thumb/9/93/Amazon_Web_Services_Logo.svg/1024px-Amazon_Web_Services_Logo.svg.png'},
+        ],
+        projectImg:'https://gruponutresa.com/wp-content/uploads/2016/09/capsulas-maquina-nutresabias-Grupo-Nutresa.jpg'
+    }
+]
